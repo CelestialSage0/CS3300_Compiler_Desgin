7e01933465110e183ab9e2b4c748372ef258e21e
Abhishek <abhishekagawai1@gmail.com>
2025-09-17 16:32:54 +0530

    Started Assignment 2

---
+++ ASSIGNMENTS/ASSIGNMENT2/Main.java
@@ -0,0 +1,21 @@
+import syntaxtree.*;
+import visitor.*;
+
+public class Main {
+   public static void main(String[] args) {
+      try {
+         Node root = new MiniJavaParser(System.in).Goal();
+         // Invoke the parser, match the Goal production and return the
+         // syntax tree.
+
+   System.out.println("Program parsed successfully");
+
+         DepthFirstVisitor v = new DepthFirstVisitor();
+         // create a visitor.
+
+         root.accept(v);
+      } catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+}
